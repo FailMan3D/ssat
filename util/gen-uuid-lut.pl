2363cec847a3420564c335295e7b9c47457e0410
FailMan3D <FailMan3D@users.noreply.github.com>
2015-05-07 09:00:21 -0700

    Changed gen-uuid-lut.pl to use ternary-if

--- util/gen-uuid-lut.pl
+++ util/gen-uuid-lut.pl
@@ -2,6 +2,7 @@
 use strict;
 print STDOUT 'insert or ignore into _sqlite_unhexhack_lookup_tbl values';
 foreach my $x (0 .. 255) {
-	printf '(\'%02X\',X\'%02X\'),', $x, $x;
-	print STDOUT !(($x - 4) % 9) ? "\n" : ' ';
+	printf '(\'%02X\',X\'%02X\')', $x, $x;
+	print STDOUT ',' if($x != 255);
+	print STDOUT ($x == 4 || ($x - 4) % 9 == 0) ? "\n" : ($x!=255)?' ':";\n";
 }
